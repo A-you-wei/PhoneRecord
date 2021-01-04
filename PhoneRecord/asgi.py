9406a0176560368bfe7db61863b735ce48d177c1
A-you-wei <yxd03200433@163.com>
2021-01-04 14:29:33 +0800

    初始化项目

--- PhoneRecord/asgi.py
+++ PhoneRecord/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for phonely project.
+ASGI config for PhoneRecord project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phonely.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PhoneRecord.settings')
 
 application = get_asgi_application()
